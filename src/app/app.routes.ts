4bd362fa350efb2f8ae36ada6d6577d838d45c3d
marcosprofile <marcos_laurindo1997@hotmail.com>
2024-03-23 19:48:58 -0300

    feat: create signup page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
-import { SignupComponent } from './pages/login/signup/signup.component';
+import { SignupComponent } from './pages/signup/signup.component';
 
 export const routes: Routes = [
   {
